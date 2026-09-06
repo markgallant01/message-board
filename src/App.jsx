a182914e7f49bb0e003243b2a7a1b709de6c3bf1
markgallant01 <markgallant01@gmail.com>
2026-09-06 05:01:04 -0400

    feat(messagePost): implement basic messagePost component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import { useState } from 'react'
 import './App.css'
 
+const MessagePost = ({ title, author, body }) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h2>{author}</h2>
+      <p>{body}</p>
+    </div>
+  )
+}
+
 function App() {
   return (
     <>
+      <MessagePost title={"Test Post"} author={"anonymous"}
+        body={"This is a test post."}/>
     </>
   )
 }
