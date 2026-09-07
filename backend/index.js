2f4274fe5b0ada2f246d1a04fe217870e683cb47
markgallant01 <markgallant01@gmail.com>
2026-09-07 05:46:01 -0400

    feat(backend): create route to serve messagePosts and make a few sample posts to test with

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,25 @@ const express = require('express')
 const app = express()
 const port = 3001
 
+const posts = [
+  {
+    title: 'Test Post',
+    author: 'anonymous',
+    body: 'This is a test post.'
+  },
+  {
+    title: 'Hmm...',
+    author: 'Siegmeyer',
+    body: 'Still closed... still closed...'
+  }
+]
+
 app.get('/', (req, res) => {
   res.send('Hello, world!')
+})
+
+app.get('/api/MessagePosts', (req, res) => {
+  res.json(posts) 
 })
 
 app.listen(port, () => {
